ad5d00cd6e4120ed3caea5ff5f9feeab29d004d5
Jirka Daněk <jirka.danek@jdanek.eu>
2023-03-11 16:02:22 +0100

    Clean up

--- plugins/extend/fosc-lib/class/Util/TableGenerator.php
+++ plugins/extend/fosc-lib/class/Util/TableGenerator.php
@@ -16,10 +16,10 @@ class TableGenerator extends Table
      */
     public static function create(
         string $id = null,
-        array $tableClasses = [],
-        array $headerColumns = null,
-        array $bodyRows = null,
-        array $footerRows = null
+        array  $tableClasses = [],
+        array  $headerColumns = null,
+        array  $bodyRows = null,
+        array  $footerRows = null
     ): Table
     {
         $instance = parent::create($id, $tableClasses);
